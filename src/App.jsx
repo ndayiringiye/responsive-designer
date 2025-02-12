492c69bbbc93fefa6266ee46ebe0b4851dc387ec
ndayiringiye david <ndayiringiyedavid120@gmail.com>
2025-02-12 12:16:26 +0200

    added  smallest larger device designer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-
 const App = () => {
   return (
-    <div className="w-11/12 mx-auto my-20">
-    <div className="grid gap-8 md:grid-cols-2 md:items-center md:text-left sm:max-xl:bg-blue-100 sm:max-xl:p-9">
+    <div className="w-11/12 mx-auto my-20 sm:bg-green-200 iphone:bg-gray-200 iphone:p-5 thin:bg-black thin:text-white thin:p-5">
+      <div className="grid gap-8 tablet:grid-cols-2 tablet:items-center tablet:text-left sm:max-xl:bg-blue-100 sm:max-xl:p-9">
         <div>
           <img className="w-full rounded-lg" src="https://images.pexels.com/photos/2244330/pexels-photo-2244330.jpeg?auto=compress&cs=tinysrgb&w=400" alt="responsive" />
         </div>
         <div>
-          <h1 className="mb-2 text-4xl font-medium">headline</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi repellat voluptate placeat? Ipsum beatae minima rerum. Adipisci molestiae quod dicta minus quas consequuntur, sequi ea mollitia, accusamus commodi laudantium qui.</p>
+          <h1 className="mb-2 text-4xl font-medium iphone:text-red-400 thin:text-lg thin:text-gray-500 iphone:text-lg iphone:text-center  sm:text-red-500">headline</h1>
+          <p className="iphone:text-blue-400 sm:text-green-500 ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi repellat voluptate placeat? Ipsum beatae minima rerum.
+          </p>
         </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "thin":"260px",
+        "iphone": "400px", // Smallest larger device
+        "tablet": "640px", 
+        "sm": "480px", // Standard small device breakpoint
+        "md": "768px", // Standard medium device
+      },
+    },
   },
   plugins: [],
 }
-
