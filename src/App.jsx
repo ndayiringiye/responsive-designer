0754f4e84f0cf3f2dec892c770bddb3c1990fe2b
ndayiringiye david <ndayiringiyedavid120@gmail.com>
2025-02-12 11:23:35 +0200

    project set up

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+const App = () => {
+  return (
+    <div className="w-11/12 mx-auto my-20">
+    <div className="grid gap-8 md:grid-cols-2 md:items-center md:text-left sm:max-xl:bg-blue-100 sm:max-xl:p-9">
+        <div>
+          <img className="w-full rounded-lg" src="https://images.pexels.com/photos/2244330/pexels-photo-2244330.jpeg?auto=compress&cs=tinysrgb&w=400" alt="responsive" />
+        </div>
+        <div>
+          <h1 className="mb-2 text-4xl font-medium">headline</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi repellat voluptate placeat? Ipsum beatae minima rerum. Adipisci molestiae quod dicta minus quas consequuntur, sequi ea mollitia, accusamus commodi laudantium qui.</p>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default App
